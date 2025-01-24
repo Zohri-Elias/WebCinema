d3dc5014221b06fa78eda60182ed9319ff84aa39
Mathis934 <m.semery@lprs.fr>
2025-01-24 08:39:46 +0100

    inscrption + connexion + formulaire qui fonctionne

---
+++ trt2(connexion).php
@@ -0,0 +1,21 @@
+<?php
+
+require_once 'Connexion.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST["email"];
+    $mdp = $_POST["mdp"];
+
+    $utilisateur = new Connexion($email, $mdp);
+
+    if ($utilisateur->verifierConnexion()) {
+        echo "<h1>Connexion réussie !</h1>";
+        echo "<p>Bienvenue, " . htmlspecialchars($email) . ".</p>";
+
+    } else {
+        echo "<h1>Échec de la connexion</h1>";
+        echo "<p>Adresse email ou mot de passe incorrect.</p>";
+        echo '<a href="connexion.html">Retour au formulaire</a>';
+    }
+}
+
--- webcinemabase.sql
+++ webcinemabase.sql
@@ -1,31 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1:3306
--- Généré le : ven. 24 jan. 2025 à 07:27
--- Version du serveur : 8.3.0
--- Version de PHP : 8.2.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de données : `webcinema`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `film`
---
 
 DROP TABLE IF EXISTS `film`;
 CREATE TABLE IF NOT EXISTS `film` (
@@ -38,11 +16,7 @@ CREATE TABLE IF NOT EXISTS `film` (
   PRIMARY KEY (`id_film`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
 
---
--- Structure de la table `reservation`
---
 
 DROP TABLE IF EXISTS `reservation`;
 CREATE TABLE IF NOT EXISTS `reservation` (
@@ -55,11 +29,6 @@ CREATE TABLE IF NOT EXISTS `reservation` (
   KEY `fk_reservation_seance` (`ref_seance`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Structure de la table `salle`
---
 
 DROP TABLE IF EXISTS `salle`;
 CREATE TABLE IF NOT EXISTS `salle` (
@@ -69,11 +38,7 @@ CREATE TABLE IF NOT EXISTS `salle` (
   PRIMARY KEY (`id_salle`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
 
---
--- Structure de la table `sceance`
---
 
 DROP TABLE IF EXISTS `sceance`;
 CREATE TABLE IF NOT EXISTS `sceance` (
@@ -88,11 +53,6 @@ CREATE TABLE IF NOT EXISTS `sceance` (
   KEY `fk_seance_film` (`ref_film`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Structure de la table `utilisateur`
---
 
 DROP TABLE IF EXISTS `utilisateur`;
 CREATE TABLE IF NOT EXISTS `utilisateur` (
@@ -105,24 +65,13 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
   PRIMARY KEY (`id_utilisateur`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Contraintes pour les tables déchargées
---
 
---
--- Contraintes pour la table `reservation`
---
+
 ALTER TABLE `reservation`
   ADD CONSTRAINT `fk_reservation_seance` FOREIGN KEY (`ref_seance`) REFERENCES `sceance` (`id_sceance`) ON DELETE CASCADE,
   ADD CONSTRAINT `fk_reservation_user` FOREIGN KEY (`ref_user`) REFERENCES `utilisateur` (`id_utilisateur`) ON DELETE CASCADE;
 
---
--- Contraintes pour la table `sceance`
---
+
 ALTER TABLE `sceance`
   ADD CONSTRAINT `fk_seance_film` FOREIGN KEY (`ref_film`) REFERENCES `film` (`id_film`) ON DELETE CASCADE;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
