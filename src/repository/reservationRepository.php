9782f0303d560b5de8a9d83b2dfdf643c8af86fc
Elias IDBRAIM <IDBRAIM_El@lprs.local>
2025-01-31 12:03:07 +0100

    modif du fichier reservationRepository.php

--- src/repository/reservationRepository.php
+++ src/repository/reservationRepository.php
@@ -1,1 +1,33 @@
 <?php
+class reservationRepository{
+
+public function ConfirmReservation(Reservation $reservation){
+    $sql = "INSERT INTO Reservation(ref_user,ref_seance,nbr_place) 
+                VALUES (:ref_user,:ref_seance,:nbr_place)";
+    $req = $this->bdd->getBdd()->prepare($sql);
+    $res = $req->execute(array(
+        'ref_user' => $reservation->getRef_user(),
+        'ref_seance' => $reservation->getRef_seance(),
+        'nbr_place' => $reservation->getNbr_place(),
+    ));
+
+    if ($res == true) {
+        return true;
+    } else {
+        return false;
+    }
+}
+public function suppressionSeance(Reservation $reservation)
+{
+    $sql = "DELETE * FROM Reservation WHERE id_reservation = :id_reservation";
+    $req = $this->bdd->getBdd()->prepare($sql);
+    $res = $req->execute(array(
+        'id_reservation' => $reservation->getId_reservation()
+    ));
+    if ($res == true) {
+        return true;
+    } else {
+        return false;
+    }
+}
+}
