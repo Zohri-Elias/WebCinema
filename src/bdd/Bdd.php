03bc31a05c019d939b10355e875d9f3084652323
Mathis934 <m.semery@lprs.fr>
2025-01-31 08:51:56 +0100

    inscription

--- src/bdd/Bdd.php
+++ src/bdd/Bdd.php
@@ -1,1 +1,14 @@
 <?php
+class Bdd {
+    public $bdd;
+
+    public function __construct($host = 'localhost', $dbname = 'webcinema', $username = 'root', $password = '') {
+
+        $this->bdd = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    }
+
+    public function getbdd() {
+        return $this->bdd;
+    }
+}
+?>
--- src/modele/Inscription.php
+++ src/modele/Inscription.php
@@ -1,102 +1,37 @@
 <?php
-
 namespace src\modele;
-class Inscription
-{
+
+require_once 'src/bdd/BDD.php';
+
+use Bdd;
+use PDO;
+
+class Inscription {
     private $nom;
-
-    /**
-     * @return mixed
-     */
-    public function getEmail()
-    {
-        return $this->email;
-    }
-
-    /**
-     * @param mixed $email
-     */
-    public function setEmail($email)
-    {
-        $this->email = $email;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getMdp()
-    {
-        return $this->mdp;
-    }
-
-    /**
-     * @param mixed $mdp
-     */
-    public function setMdp($mdp)
-    {
-        $this->mdp = $mdp;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getPrenom()
-    {
-        return $this->prenom;
-    }
-
-    /**
-     * @param mixed $prenom
-     */
-    public function setPrenom($prenom)
-    {
-        $this->prenom = $prenom;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getNom()
-    {
-        return $this->nom;
-    }
-
-    /**
-     * @param mixed $nom
-     */
-    public function setNom($nom)
-    {
-        $this->nom = $nom;
-    }
-
     private $prenom;
-
     private $email;
     private $mdp;
 
-    public function __construct($nom, $prenom, $email, $mdp)
-    {
-        $this->setNom($nom);
-        $this->setPrenom($prenom);
-        $this->setEmail($email);
-        $this->setMdp($mdp);
-
+    public function __construct($nom, $prenom, $email, $mdp) {
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->email = $email;
+        $this->mdp = $mdp;
     }
 
+    public function inscrire() {
+        $bdd = new Bdd();
+        $connection = $bdd->getbdd();
 
-    public function inscrire()
-    {
-        $bdd = new PDO('mysql:host=localhost;dbname=webcinema', 'root', '');
-        $req = $bdd->prepare('INSERT INTO utilisateur (nom , prenom , email , mdp) VALUES (:prenom, :nom, :email , :mdp)');
-        $req->execute(array(
-            'nom' => $this->getNom(),
-            'prenom' => $this->getPrenom(),
-            'email' => $this->getEmail(),
-            'mdp' => $this->getMdp()
 
-        ));
+        $hashedPassword = password_hash($this->mdp, PASSWORD_DEFAULT);
+        $req = $connection->prepare('INSERT INTO utilisateur (nom, prenom, email, mdp) VALUES (:nom, :prenom, :email, :mdp)');
+        $req->execute([
+            'nom' => $this->nom,
+            'prenom' => $this->prenom,
+            'email' => $this->email,
+            'mdp' => $hashedPassword
+        ]);
     }
 }
-
-
-
+?>
