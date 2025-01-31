10b5db924d1d193588af653454db5874e961a76b
Elias ZOHRI CHAKKOUF <ZOHRI-CHAKKOUF_El@lprs.local>
2025-01-31 10:34:22 +0100

    mis a jour

--- src/bdd/Bdd.php
+++ src/bdd/Bdd.php
@@ -5,6 +5,7 @@ class Bdd {
     public function __construct($host = 'localhost', $dbname = 'webcinema', $username = 'root', $password = '') {
 
         $this->bdd = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        $this->bdd->exec("set names utf8");
     }
 
     public function getbdd() {
