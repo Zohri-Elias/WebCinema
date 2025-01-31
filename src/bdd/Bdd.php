23ce144ddd9ec6168a3cb53d68d07320e65ff1b3
Elias ZOHRI CHAKKOUF <ZOHRI-CHAKKOUF_El@lprs.local>
2025-01-31 10:30:25 +0100

    Structuration du catalogue

--- src/bdd/Bdd.php
+++ src/bdd/Bdd.php
@@ -5,10 +5,13 @@ class Bdd {
     public function __construct($host = 'localhost', $dbname = 'webcinema', $username = 'root', $password = '') {
 
         $this->bdd = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        $this->bdd->exec("set names utf8");
     }
 
     public function getbdd() {
         return $this->bdd;
     }
+
+
 }
 ?>
---
+++ src/repository/FilmRepository.php
@@ -0,0 +1,31 @@
+<?php
+
+use src\modele\Film;
+
+require_once $_SERVER['DOCUMENT_ROOT'] . "/exo/WebCinema/src/bdd/Bdd.php";
+
+class FilmRepository
+{
+
+    public function afficherCatalogue()
+    {
+        $films=[];
+        $bdd = new Bdd();
+        $connexion = $bdd->getbdd();
+        $filmsBdd = $connexion->query("SELECT * FROM film ORDER BY id_film")->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($filmsBdd as $film) {
+            $films[]= new Film([
+                "idFilm"=>$film['id_film'],
+                "nomFilm"=>$film['nom_film'],
+                "duree"=>$film['duree'],
+                "genre"=>$film['genre'],
+                "description"=>$film['description'],
+                "image"=>$film['image'],
+
+            ]);
+        }
+        return $films;
+    }
+}
+// port=3307;
+?>
