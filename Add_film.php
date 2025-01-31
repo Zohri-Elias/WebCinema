388f9813ce7b4c3908e9322bd1fd6030ef2958b3
Touzanine Issa <i.touzanine@lprs.fr>
2025-01-31 12:01:14 +0100

    fix : resolution de probleme ajout de film V1

--- Add_film.php
+++ Add_film.php
@@ -1,37 +1,36 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "webcinema";
+
 $bdd = new PDO('mysql:host=localhost;dbname=webcinema;charset=utf8', 'root', '');
+
 $req = $bdd->prepare('INSERT INTO film (nom_film, duree, genre, description, image) VALUES(:nom_film, :duree, :genre, :description, :image)');
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nom_film = $_POST['film-title'];
+    $duree = $_POST['film-duration'];
+    $genre = $_POST['film-genre'];
     $description = $_POST['film-description'];
 
-    $image = $_FILES['film-image']['name'];
-    $image_tmp = $_FILES['film-image']['tmp_name'];
-    $image_path = "uploads/" . basename($image);
+    if ($_FILES['film-image']['error'] == 0) {
+        $image = $_FILES['film-image']['name'];
+        $image_tmp = $_FILES['film-image']['tmp_name'];
+        $image_path = "uploads/" . basename($image);
 
-    if (move_uploaded_file($image_tmp, $image_path)) {
-        $sql = "INSERT INTO film (nom_film, description, image_url) VALUES ('$nom_film', '$description', '$image_path')";
+        if (move_uploaded_file($image_tmp, $image_path)) {
+            $req->execute([
+                'nom_film' => $nom_film,
+                'duree' => $duree,
+                'genre' => $genre,
+                'description' => $description,
+                'image' => $image_path
+            ]);
 
-        if ($conn->query($sql) === TRUE) {
-            echo "Film ajouté avec succès.";
+            echo "Film ajouté avec succès!";
         } else {
-            echo "Erreur : " . $sql . "<br>" . $conn->error;
+            echo "Erreur lors de l'upload de l'image.";
         }
     } else {
-        echo "Erreur lors de l'upload de l'image.";
+        echo "Aucune image téléchargée.";
     }
 }
 
-$conn->close();
 ?>
